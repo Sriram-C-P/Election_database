914f828406c8d6a7f85666dab72bdefd02122a11
Sriram C P <145141253+Sriram-C-P@users.noreply.github.com>
2026-07-29 18:23:58 +0530

    change structure

--- Election_website/DBMS_microproject-main/static/js/base.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const alertCloseButtons = document.querySelectorAll('.btn-close');
-    alertCloseButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            this.parentElement.style.display = 'none';
-        });
-    });
-});
